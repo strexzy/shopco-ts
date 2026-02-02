2e25be0f26cbb65f2a387944f5827a8e2c3bb251
Roman Kh <strex.gamer91@gmail.com>
2026-02-03 01:40:21 +0500

    Refactored succesfully

--- app/shared/lib/refresh.ts
+++ app/shared/lib/refresh.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { AuthToken } from "~/features/auth/types/auth.types";
+import type { AuthToken } from "~/features";
 import { API_URL, AUTH_ENDPOINTS } from "~/shared/constants/constants";
 
 export const refreshToken = async () =>
